8791f8772e759cf2880b82a3b9b7029fb7ad3b17
James Harris <james@rancher.com>
2015-05-28 10:42:09 -0700

    Better restart.

--- restartRanchDev.sh
+++ restartRanchDev.sh
@@ -8,7 +8,9 @@ cd $(dirname $0)/
 boot2Docker up 1> /dev/null
 $(boot2Docker shellinit 1> /dev/null)
 mysql.server start 1> /dev/null
+
 mysql -uroot < ./delDatabase.sql 1> /dev/null
-mysql -uroot < $(echo $CATTLE_ROOT/resources/content/db/mysql/create_db_and_user_dev.sql) 1> /dev/null
+cd ${CATTLE_ROOT:=${HOME}/code/rancherio/cattle}
+mysql -uroot < ./resources/content/db/mysql/create_db_and_user_dev.sql 1> /dev/null
 
 echo Restarted
