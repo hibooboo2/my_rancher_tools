05a31f0f96a95a8a749625598b233f0c9833060e
James Jeffrey <james@rancher.com>
2015-11-03 14:33:48 -0700

    Changes to where rancher is..

--- restartRanchDev.sh
+++ restartRanchDev.sh
@@ -5,13 +5,9 @@ set -ex
 cd $(dirname $0)/
 ./safteyChecks.sh
 
-boot2Docker up 1> /dev/null
-boot2Docker restart 1> /dev/null
-$(boot2Docker shellinit 1> /dev/null)
 mysql.server start 1> /dev/null
 
 mysql -uroot < ./delDatabase.sql 1> /dev/null
-cd ${CATTLE_ROOT:=${HOME}/code/rancherio/cattle}
+cd ${CATTLE_ROOT:=${HOME}/code/rancher/cattle}
 mysql -uroot < ./resources/content/db/mysql/create_db_and_user_dev.sql 1> /dev/null
-
 echo Restarted
